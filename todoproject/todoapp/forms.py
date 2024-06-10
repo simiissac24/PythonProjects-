07b4f9111a280c8b79c771038da1fc6b2740a1fe
simiissac24 <simimias@gmail.com>
2024-06-10 13:13:39 +0100

    addedd

---
+++ todoproject/todoapp/forms.py
@@ -0,0 +1,12 @@
+
+
+        # todoapp/forms.py
+from django import forms
+from .models import Task
+
+class TodoForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['name', 'priority', 'date']
+
+
